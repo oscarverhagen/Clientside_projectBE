60ef2e6a5b8b69d3ba15e6972e122d32188a4c61
OscarVerhagen <58423620+oscarverhagen@users.noreply.github.com>
2023-04-07 20:54:35 +0200

    test

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 // reads the .env file and stores it as environment variables, use for config
 require('dotenv').config()
 
-//const connect = require('./connect')
-
+// const connect = require('./connect')
 const mongoMemoryServer = require('./src/mongomemoryserver')
 const app = require('./src/app')
 
-// the order ofnpm  starting the app and connecting to the database does not matter
+// the order of starting the app and connecting to the database does not matter
 // since mongoose buffers queries till there is a connection
 
 // start the app
@@ -14,10 +13,11 @@ const port = process.env.PORT
 app.listen(port, () => {
     console.log(`server is listening on port ${port}`)
 })
-// app.get("/", (req, res) => {
-//     res.json({message: "Welcome to the Flexplex backend!"});
-// });
+
 // connect to the databases
+// connect.mongo(process.env.MONGO_PROD_DB)
+// connect.neo(process.env.NEO4J_PROD_DB)
+
 
 mongoMemoryServer.connect();
-//mongoMemoryServer.seedTables();
+
